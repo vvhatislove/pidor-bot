f987fa15b54291b61a19b5dcdd71d2a258adfc18
VVhatislove <petrenkoazaza@gmail.com>
2021-12-23 15:25:23 +0200

    еще раз удалил конфиг

--- config.py
+++
@@ -1,7 +0,0 @@
-TOKEN = "2062354496:AAEiMmUjIvXdiAfGKNslyyc-WNXbnGEuJMI"
-DATABASE = "d8uf9faqtc63s3"
-USER = "szkgmutwvkegga"
-PASSWORD = "b4240c3423193fcf5970ae838b206dbe6b60d313c4e932dd3d3d8adb74141b38"
-HOST = "ec2-54-74-95-84.eu-west-1.compute.amazonaws.com"
-PORT = 5432
-adminId = 474625366
