eaf2f9747676fa5c389ff6eac42af04d2a992cb9
Vladislav Petrenko <petrenkoazaza@gmail.com>
2025-05-26 20:57:08 +0300

    fix trigger for old messages

--- handlers/triggers.py
+++ handlers/triggers.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timezone
+
 from aiogram import Router, F
 from aiogram.types import Message
 
@@ -11,20 +13,28 @@ async def trigger_handler(message: Message):
     if message.chat.type == "private":
         await message.answer(CommandText.WRONG_CHAT)
         return
+    message_ttl_seconds = 60
+    # Проверка на "устаревшее" сообщение
+    message_time = datetime.fromtimestamp(message.date.timestamp(), tz=timezone.utc)
+    now = datetime.now(timezone.utc)
+    delta_seconds = (now - message_time).total_seconds()
+    if delta_seconds > message_ttl_seconds:
+        return  # старое сообщение — игнорируем
+
     if message.text:
         text = message.text
         if not isinstance(text, str):
             return
-        else:
-            try:
-                text = str(text)
-            except Exception as e:
-                print(f"Ошибка преобразования в строку: {e}")
-                return
-            for trigger in GameText.TRIGGERS:
-                if trigger in text:
-                    trigger_message = await AIService.get_response(text, AIPromt.PIDOR_TRIGGERS_PROMT)
-                    await message.reply(trigger_message)
-                    break  # чтобы не отвечать на несколько триггеров одновременно
+        try:
+            text = str(text)
+        except Exception as e:
+            print(f"Ошибка преобразования в строку: {e}")
+            return
+
+        for trigger in GameText.TRIGGERS:
+            if trigger in text:
+                trigger_message = await AIService.get_response(text, AIPromt.PIDOR_TRIGGERS_PROMT)
+                await message.reply(trigger_message)
+                break
 
 
