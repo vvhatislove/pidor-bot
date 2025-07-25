3c1a0bb691e23e97b89e149f4a93039a999ba736
vladislav <petrenkoazaza@gmail.com>
2025-07-25 14:44:12 +0300

    deleted debug message

--- handlers/user/auto_pidor.py
+++ handlers/user/auto_pidor.py
@@ -27,7 +27,6 @@ async def cmd_auto_pidor(message: Message, session: AsyncSession):
 
     chat.auto_pidor = not chat.auto_pidor
     await session.commit()
-    await message.answer(str(await ChatCRUD.get_auto_pidor_chats(session)))
 
     status = "✅ Автоматическое определение пидора дня активно В 12:00 каждый день!" if chat.auto_pidor else "❌ Автоматическое определение пидора дня деактивировано"
     logger.info(f"Auto-pidor toggled to {chat.auto_pidor} in chat {chat_id}")
